776264ed3bd08b3874c56f654c0742e398fa66a6
WiiPlayer2 <waldetomme@gmail.com>
2014-03-27 09:51:05 +0100

    Fixed style issues in ObjectExtension.cs

--- DarkLib/ObjectExtension.cs
+++ DarkLib/ObjectExtension.cs
@@ -1,19 +1,39 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="ObjectExtension.cs" company="DarkInc">
+//     Copyright (c) DarkInc, WiiPlayer2 (Waldemar Tomme). All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
 using System.Diagnostics;
 using System.Reflection;
 
 namespace System
 {
+    /// <summary>
+    /// Contains extension methods for <see cref="System.Object"/>.
+    /// </summary>
     public static class ObjectExtension
     {
-        public const BindingFlags ALL_MEMBERS = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+        /// <summary>
+        /// The constant value for all members.
+        /// </summary>
+        public const BindingFlags AllMembers = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
 
+        /// <summary>
+        /// Debugs the fields.
+        /// </summary>
+        /// <param name="object">The object.</param>
         [Conditional("DEBUG")]
         public static void DebugFields(this object @object)
         {
             DebugFields(@object, BindingFlags.Instance | BindingFlags.Public);
         }
 
+        /// <summary>
+        /// Debugs the fields.
+        /// </summary>
+        /// <param name="object">The object.</param>
+        /// <param name="bindingAttr">The binding attribute.</param>
         [Conditional("DEBUG")]
         public static void DebugFields(this object @object, BindingFlags bindingAttr)
         {
@@ -24,12 +44,21 @@ namespace System
             }
         }
 
+        /// <summary>
+        /// Debugs the properties.
+        /// </summary>
+        /// <param name="object">The object.</param>
         [Conditional("DEBUG")]
         public static void DebugProperties(this object @object)
         {
             DebugProperties(@object, BindingFlags.Instance | BindingFlags.Public);
         }
 
+        /// <summary>
+        /// Debugs the properties.
+        /// </summary>
+        /// <param name="object">The object.</param>
+        /// <param name="bindingAttr">The binding attribute.</param>
         [Conditional("DEBUG")]
         public static void DebugProperties(this object @object, BindingFlags bindingAttr)
         {
