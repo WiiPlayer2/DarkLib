51a448ecde6802dc27acf13b536ef2be3270086d
WiiPlayer2 <waldetomme@gmail.com>
2014-03-27 09:41:20 +0100

    Fixed style issues in TimerExtension.cs

--- DarkLib/Timers/TimerExtension.cs
+++ DarkLib/Timers/TimerExtension.cs
@@ -1,15 +1,28 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="TimerExtension.cs" company="DarkInc">
+//     Copyright (c) DarkInc, WiiPlayer2 (Waldemar Tomme). All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
 
 namespace System.Timers
 {
+    /// <summary>
+    /// Contains extension methods for <see cref="System.Timers.Timer"/>
+    /// </summary>
     public static class TimerExtension
     {
-        public static void SetElapsedDate(this Timer @Timer, DateTime dateTime)
+        /// <summary>
+        /// Sets the elapsed date.
+        /// </summary>
+        /// <param name="timer">The timer.</param>
+        /// <param name="dateTime">The date time.</param>
+        public static void SetElapsedDate(this Timer timer, DateTime dateTime)
         {
             var now = DateTime.UtcNow;
             var utc = dateTime.ToUniversalTime();
             var diff = utc - now;
-            @Timer.Interval = diff.TotalMilliseconds;
+            timer.Interval = diff.TotalMilliseconds;
         }
     }
 }
