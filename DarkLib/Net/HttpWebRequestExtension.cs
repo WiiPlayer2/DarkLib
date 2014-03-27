714637092d202f2d96882133f3cfead781da5adb
WiiPlayer2 <waldetomme@gmail.com>
2014-03-27 09:35:40 +0100

    Fixed style issues in HttpWebRequestExtension.cs

--- DarkLib/Net/HttpWebRequestExtension.cs
+++ DarkLib/Net/HttpWebRequestExtension.cs
@@ -1,20 +1,36 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="HttpWebRequestExtension.cs" company="DarkInc">
+//     Copyright (c) DarkInc, WiiPlayer2 (Waldemar Tomme). All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
 
 namespace System.Net
 {
+    /// <summary>
+    /// Contains extension methods for <see cref="System.Net.HttpWebRequest"/>
+    /// </summary>
     public static class HttpWebRequestExtension
     {
-        public static HttpWebResponse GetResponseNoException(this HttpWebRequest @HttpWebRequest)
+        /// <summary>
+        /// Gets the response without an exception.
+        /// </summary>
+        /// <param name="httpWebRequest">The HTTP web request.</param>
+        /// <returns>The Http web response.</returns>
+        public static HttpWebResponse GetResponseNoException(this HttpWebRequest httpWebRequest)
         {
             try
             {
-                return (HttpWebResponse)@HttpWebRequest.GetResponse();
+                return (HttpWebResponse)httpWebRequest.GetResponse();
             }
             catch (WebException we)
             {
                 var resp = we.Response as HttpWebResponse;
                 if (resp == null)
+                {
                     throw;
+                }
+
                 return resp;
             }
         }
